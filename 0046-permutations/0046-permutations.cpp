849b8abd8acf6c6c218c8363229ff4607af7abb2
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-05-09 12:34:37 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.32%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    void solve(int j, vector<int>& nums, vector<vector<int>>& ans) {
-        if (j == nums.size()) {
+    void solve(int i, vector<int>& nums, vector<vector<int>>& ans) {
+        // int n = nums.size();
+        if (i == nums.size()) {
             ans.push_back(nums);
             return;
         }
-        for (int i = j; nums.size() > i; i++) {
+
+        for (int j = i; nums.size() > j; j++) {
             swap(nums[i], nums[j]);
-            solve(j + 1, nums, ans);
+            solve(i + 1, nums, ans);
             swap(nums[i], nums[j]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
+        // ans
         vector<vector<int>> ans;
-        // ans.push_back(nums);
+        // int n = nums.size();
         solve(0, nums, ans);
         return ans;
     }
