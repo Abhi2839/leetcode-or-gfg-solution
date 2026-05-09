6a0f8a541cac0a7b51bfe55a89f2b0e8e50fdee0
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-05-09 11:07:07 +0530

    Time: 0 ms (100%), Space: 10.5 MB (7.08%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
@@ -3,16 +3,12 @@ public:
     bool isFascinating(int n) {
         int se = 2 * n;
         int third = 3 * n;
-
         string s = to_string(n) + to_string(se) + to_string(third);
-
         unordered_map<char, int> mp;
         for (auto x : s)
             mp[x]++;
-
         if (s.length() != 9)
             return 0;
-
         if (s.length() == 9) {
             for (int i = 0; s.length() > i; i++) {
                 if (mp[s[i]] > 1 or mp[s[i]] < 1 or s[i]=='0')
