3eca594d6b171b806690b8703978142e9cf7b00d
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-05-11 13:53:56 +0530

    Time: 0 ms (100%), Space: 193.5 MB (11.55%) - LeetHub

--- 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.cpp
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int minimumPrefixLength(vector<int>& nums) {
-        int n = nums.size() - 1;
-        while (n > 0 and nums[n] > nums[n - 1])
-            n--;
-        return n;
+        // traverse from n-1 and compare n-1 > n-2 i>0;
+        // return i;
+        int i = nums.size() - 1;
+        while (i > 0 and nums[i] > nums[i - 1])
+            i--;
+        return i;
     }
 };
