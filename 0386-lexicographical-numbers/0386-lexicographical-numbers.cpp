3634f352d7f9e64066b37b9ba1d0cd3a0727fb73
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-05-13 09:00:58 +0530

    Time: 0 ms (100%), Space: 14.1 MB (63.13%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -5,12 +5,26 @@ public:
         //  first 1 10 11 12 13 14
         // converting into str
 
-        vector<string> temp;
-        for (int i = 1; n >= i; i++)
-            temp.push_back(to_string(i));
-        sort(temp.begin(), temp.end());
-        for (auto x : temp)
-            ans.push_back(stoi(x));
+        // vector<string> temp;
+        // for (int i = 1; n >= i; i++)
+        //     temp.push_back(to_string(i));
+        // sort(temp.begin(), temp.end());
+        // for (auto x : temp)
+        //     ans.push_back(stoi(x));
+
+        //  with constant space
+
+        int ele = 1;
+        for (int i = 0; n > i; i++) {
+            ans.push_back(ele);
+            if (ele * 10 <= n)
+                ele *= 10;
+            else {
+                while (ele % 10 == 9 or ele + 1 > n)
+                    ele /= 10;
+                ele++;
+            }
+        }
 
         return ans;
     }
