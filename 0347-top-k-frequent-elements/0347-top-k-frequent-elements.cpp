4e5e0b91405c1480230b16e18ed955d1964ccb02
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-05-15 21:45:04 +0530

    Time: 0 ms (100%), Space: 17.9 MB (83.72%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+
+        for (auto x : nums)
+            mp[x]++;
+        for (auto& it : mp) {
+            pq.push({it.second, it.first});
+            if (pq.size() > k)
+                pq.pop();
+        }
+        while (k--) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
