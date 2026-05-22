171142575b2a65c34741d961dde078eed00d7790
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-05-22 11:25:22 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.81%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int key) {
+        int l = 0;
+        int h = arr.size() - 1;
+        
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            
+            if (arr[mid] == key)
+                return mid;
+            if (arr[l] <= arr[mid]) {
+                if (key >= arr[l] && key < arr[mid])
+                    h = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            else {
+                if (key > arr[mid] && key <= arr[h])
+                    l = mid + 1;
+                else
+                    h = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
