b86c2eb558483e493d3aefc654f39dbeb175a856
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-06-04 21:24:05 +0530

    Time: 55 ms (33.96%), Space: 10.3 MB (33.96%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -4,7 +4,7 @@ public:
         string s = to_string(num);
         int cnt = 0;
 
-        for (int i = 1; i < (int)s.size() - 1; i++) {
+        for (int i = 1; i <s.size() - 1; i++) {
             if ((s[i] > s[i - 1] && s[i] > s[i + 1]) ||
                 (s[i] < s[i - 1] && s[i] < s[i + 1])) {
                 cnt++;
