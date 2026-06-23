c0ae6c0cd179ca9a34777afb36a500f46786d814
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-06-23 14:57:33 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int cnt = 0, ele = -1;
+        int cnt = 0, ele = 0;
         for (auto x : nums) {
             if (cnt == 0) {
+                cnt++;
                 ele = x;
-                // cnt++;
+                continue;
             }
             if (ele == x)
                 cnt++;
             else
                 cnt--;
         }
-        cnt = 0;
-        // for checking if we found the element that has freq n/2
-        for (auto x : nums)
-            if (ele == x)
-                cnt++;
-
-        return (cnt > nums.size() / 2) ? ele : -1;
+        return ele;
     }
 };
