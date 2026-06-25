37310270357a3c1da1500f75186a2ad09a0761b4
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-06-25 10:54:18 +0530

    Time: 0 ms (100%), Space: 34.4 MB (98.01%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -4,7 +4,8 @@ public:
         int cnt = 0;
         // store rem in hashmap using prefix sum
         int sum = 0;
-        unordered_map<int, int> mp;
+        // unordered_map<int,int>mp;
+        vector<int>mp(k,0);
         mp[0] = 1; // base case
         for (int i = 0; nums.size() > i; i++) {
             sum = ((sum + nums[i]) % k + k) % k;
