d3ac70fa09b8ced5e5cf031a7fe8c05c88b307c0
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-06-28 08:47:43 +0530

    Time: 15 ms (12.43%), Space: 55.3 MB (36.72%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -3,11 +3,10 @@ public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         sort(arr.begin(), arr.end());
         arr[0] = 1;
-        int n = arr.size();
-        for (int i = 1; n > i; i++) {
-            if (arr[i] - arr[i - 1] > 1)
+        for (int i = 1; arr.size() > i; i++)
+            if (arr[i] > arr[i - 1])
                 arr[i] = arr[i - 1] + 1;
-        }
-        return arr[n - 1];
+
+        return arr[arr.size() - 1];
     }
 };
