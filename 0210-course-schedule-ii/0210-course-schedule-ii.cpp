f3ef8c5257c0e505044948eb38bb20faebb21f60
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-03 20:53:43 +0530

    Time: 0 ms (100%), Space: 18.2 MB (70.86%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& arr) {
+        // return topological sort
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+        for (auto& a : arr)
+            adj[a[1]].push_back(a[0]);
+
+        for (int i = 0; n > i; i++) {
+            for (auto x : adj[i])
+                indegree[x]++;
+        }
+        queue<int> qu;
+        for (int i = 0; n > i; i++)
+            if (indegree[i] == 0)
+                qu.push(i);
+        vector<int> topo;
+
+        while (!qu.empty()) {
+            int curr = qu.front();
+            qu.pop();
+            topo.push_back(curr);
+            for (auto x : adj[curr]) {
+                indegree[x]--;
+                if (indegree[x] == 0) {
+                    qu.push(x);
+                }
+            }
+        }
+        if (topo.size()!=n) return {};
+        return topo;
+    }
+};
