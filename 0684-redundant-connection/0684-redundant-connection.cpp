b416d8d14e071d8ffe09c2be9eb71046ea944d58
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-09 21:33:55 +0530

    Time: 0 ms (100%), Space: 13.2 MB (71.87%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    int find(int i, vector<int>& parent) {
-        if (parent[i] == i) {
-            return i;
-        }
-        return parent[i] = find(parent[i], parent);
+    int find(int node, vector<int>& parent) {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = find(parent[node], parent);
     }
-    void unite(vector<int>& parent, vector<int>& size, int u, int v) {
-
-        int parU = find(u, parent);
-        int parV = find(v, parent);
-        if (parU == parV)
-            return;
-        if (size[parU] >= size[parV]) {
-            parent[parV] = parU;
-            size[parU] += size[parV];
+    void unite(int u, int v, vector<int>& parent, vector<int>& size) {
+        int paru = find(u, parent);
+        int parv = find(v, parent);
+        if (size[paru] >= size[parv]) {
+            parent[parv] = paru;
+            size[paru] += parv;
         } else {
-            parent[parU] = parV;
-            size[parV] += size[parU];
+            parent[paru] = parv;
+            size[parv] += paru;
         }
-
-        // parent[u] = v;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> parent(n + 1);
-        for (int i = 0; n >= i; i++)
+        vector<int> parent(n+1);
+        for (int i = 0; n >= i; i++) {
             parent[i] = i;
+        }
         vector<int> size(n + 1, 1);
         for (auto& e : edges) {
             if (find(e[0], parent) == find(e[1], parent))
                 return e;
-            unite(parent, size, e[0], e[1]);
+            unite(e[0], e[1], parent, size);
         }
         return {-1, -1};
     }
