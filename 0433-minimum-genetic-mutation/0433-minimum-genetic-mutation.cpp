163bcc0b93876c8217d56fba2e700afcd53c2d30
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-10 14:21:33 +0530

    Time: 0 ms (100%), Space: 9 MB (46.93%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int minMutation(string s, string e, vector<string>& bank) {
-        unordered_set<string> st(bank.begin(), bank.end());
+    int minMutation(string s, string e, vector<string>& arr) {
+        unordered_set<string> st(arr.begin(), arr.end());
         queue<pair<string, int>> qu;
-
-        char arr[4] = {'A', 'G', 'T', 'C'};
         qu.push({s, 0});
-
+        char se[4] = {'A', 'C', 'G', 'T'};
         while (!qu.empty()) {
             auto [word, step] = qu.front();
             qu.pop();
             if (word == e)
                 return step;
+
             for (int i = 0; word.length() > i; i++) {
                 char ch = word[i];
                 for (int k = 0; 4 > k; k++) {
-                    word[i] = arr[k];
-                    if (st.count(word) > 0) {
-                        qu.push({word, step + 1});
+                    word[i] = se[k];
+                    if (st.count(word)) {
                         st.erase(word);
+                        qu.push({word, step + 1});
                     }
-                    word[i] = ch;
                 }
+                word[i] = ch;
             }
         }
         return -1;
