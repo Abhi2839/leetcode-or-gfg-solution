7c51e51c51a8f1e010c084175b81b91d88690cf6
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-11 09:46:49 +0530

    Time: 41 ms (67.76%), Space: 130.7 MB (59.64%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj, vector<int>& vis, int j,
+    void dfs(vector<vector<int>>& adj, vector<int>& vis, int node,
              vector<int>& comp) {
 
-        vis[j] = 1;
-        comp.push_back(j);
+        vis[node] = 1;
+        comp.push_back(node);
 
-        for (int nxt : adj[j]) {
+        for (int nxt : adj[node]) {
             if (!vis[nxt])
                 dfs(adj, vis, nxt, comp);
         }
     }
-int countCompleteComponents(int n, vector<vector<int>>& edges) {
+
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
 
         vector<vector<int>> adj(n);
-        for (auto& e : edges) {
+
+        for (auto &e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
 
         vector<int> vis(n, 0);
-        int cnt = 0;
+        int ans = 0;
 
         for (int i = 0; i < n; i++) {
 
@@ -30,20 +32,20 @@ int countCompleteComponents(int n, vector<vector<int>>& edges) {
                 dfs(adj, vis, i, comp);
 
                 int sz = comp.size();
-                bool ans = true;
+                bool complete = true;
 
-                for (int j : comp) {
-                    if (adj[j].size() != sz - 1) {
-                        ans = false;
+                for (int node : comp) {
+                    if (adj[node].size() != sz - 1) {
+                        complete = false;
                         break;
                     }
                 }
 
-                if (ans)
-                    cnt++;
+                if (complete)
+                    ans++;
             }
         }
 
-        return cnt;
+        return ans;
     }
 };
