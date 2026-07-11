13573ff44219b4ee5f7ca844ee1f60622ef69f32
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-11 09:47:12 +0530

    Time: 47 ms (57.53%), Space: 130.7 MB (57.98%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj, vector<int>& vis, int node,
+    void dfs(vector<vector<int>>& adj, vector<int>& vis, int j,
              vector<int>& comp) {
 
-        vis[node] = 1;
-        comp.push_back(node);
+        vis[j] = 1;
+        comp.push_back(j);
 
-        for (int nxt : adj[node]) {
+        for (int nxt : adj[j]) {
             if (!vis[nxt])
                 dfs(adj, vis, nxt, comp);
         }
     }
-
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+int countCompleteComponents(int n, vector<vector<int>>& edges) {
 
         vector<vector<int>> adj(n);
-
-        for (auto &e : edges) {
+        for (auto& e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
 
         vector<int> vis(n, 0);
-        int ans = 0;
+        int cnt = 0;
 
         for (int i = 0; i < n; i++) {
 
@@ -32,20 +30,20 @@ public:
                 dfs(adj, vis, i, comp);
 
                 int sz = comp.size();
-                bool complete = true;
+                bool ans = true;
 
-                for (int node : comp) {
-                    if (adj[node].size() != sz - 1) {
-                        complete = false;
+                for (int j : comp) {
+                    if (adj[j].size() != sz - 1) {
+                        ans = false;
                         break;
                     }
                 }
 
-                if (complete)
-                    ans++;
+                if (ans)
+                    cnt++;
             }
         }
 
-        return ans;
+        return cnt;
     }
 };
