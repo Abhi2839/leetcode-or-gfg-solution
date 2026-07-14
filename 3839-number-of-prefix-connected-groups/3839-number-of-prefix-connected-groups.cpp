24268916d07ad21b39a4d96d5f178cb4f6cebb90
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-14 18:59:30 +0530

    Time: 100 ms (17.68%), Space: 147.1 MB (5.13%) - LeetHub

--- 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.cpp
+++ 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int prefixConnected(vector<string>& words, int k) {
-        //  unorered mp
-        //  first element will store first k char
-        // second element complete word
-        // /then count the elements in second min 2
-
-        unordered_map<string, vector<string>> ans;
-
-        for (auto& x : words) {
-
-            if (x.size() < k)
-                continue;
-
-            ans[x.substr(0, k)].push_back(x);
-        }
-
         int cnt = 0;
-
-        for (auto& it : ans) {
-
+        unordered_map<string, vector<string>> mp;
+        for (auto x : words) {
+            if (x.length() < k)
+                continue;
+            mp[x.substr(0, k)].push_back(x);
+        }
+        for (auto& it : mp) {
             if (it.second.size() >= 2)
                 cnt++;
         }
-
         return cnt;
     }
 };
