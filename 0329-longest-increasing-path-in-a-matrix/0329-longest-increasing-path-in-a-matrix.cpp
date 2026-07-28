e92baeb9f103bcd6c243403704bec89c7d487b81
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-28 21:15:09 +0530

    Time: 3 ms (96.26%), Space: 19.1 MB (97.01%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& arr, int i, int j, vector<vector<int>>& dp) {
+int dp[201][201];
+    int dfs(vector<vector<int>>& arr, int i, int j) {
         int m = arr.size();
         int n = arr[0].size();
         int dr[4] = {-1, 0, 1, 0};
@@ -14,7 +15,7 @@ public:
             int nc = j + dc[k];
             if (nr >= 0 and m > nr and nc >= 0 and n > nc and
                 arr[nr][nc] > arr[i][j]) {
-                ans = max(ans, 1 + dfs(arr, nr, nc, dp));
+                ans = max(ans, 1 + dfs(arr, nr, nc));
             }
         }
         return dp[i][j] = ans;
@@ -22,11 +23,12 @@ public:
     int longestIncreasingPath(vector<vector<int>>& arr) {
         int m = arr.size();
         int n = arr[0].size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        memset(dp,-1,sizeof(dp));
         int ans = 0;
         for (int i = 0; m > i; i++) {
             for (int j = 0; n > j; j++) {
-                ans = max(ans, 1 + dfs(arr, i, j, dp));
+                ans = max(ans, 1 + dfs(arr, i, j));
             }
         }
         return ans;
