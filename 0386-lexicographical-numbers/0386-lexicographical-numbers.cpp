3c1c00be0f6a8a70770624bb3bae9ff9c99c6a32
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-30 00:17:24 +0530

    Time: 73 ms (13.46%), Space: 28.2 MB (9.02%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         vector<int> ans;
-        // using o(n) space
+        //  first 1 10 11 12 13 14
+        // converting into str
 
-        // vector<string> temp;
-        // for (int i = 1; n >= i; i++)
-        //     temp.push_back(to_string(i));
-        // sort(temp.begin(), temp.end());
+        vector<string> temp;
+        for (int i = 1; n >= i; i++)
+            temp.push_back(to_string(i));
+        sort(temp.begin(), temp.end());
+        for (auto x : temp)
+            ans.push_back(stoi(x));
 
-        // for (auto x : temp)
-        //     ans.push_back(stoi(x));
+        //  with constant space
 
-        // using constant space
-        int ele = 1;
-        for (int i = 0; n > i; i++) {
-            ans.push_back(ele);
+        // int ele = 1;
+        // for (int i = 0; n > i; i++) {
+        //     ans.push_back(ele);
+        //     if (ele * 10 <= n)
+        //         ele *= 10;
+        //     else {
+        //         while (ele % 10 == 9 or ele + 1 > n)
+        //             ele /= 10;
+        //         ele++;
+        //     }
+        // }
 
-            if (n >= ele * 10)
-                ele *= 10;
-            else {
-                while (ele % 10 == 9 or ele + 1 > n)
-                    ele /= 10;
-                ele++;
-            }
-        }
         return ans;
     }
 };
