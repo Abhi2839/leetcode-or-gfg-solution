c73b8fe125ca8db0876b25ba7c1bc1d640c4fe13
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-07-31 19:19:42 +0530

    Time: 5 ms (90.47%), Space: 9.4 MB (77.8%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,31 +1,38 @@
+
 class Solution {
 public:
     string longestPalindrome(string s) {
-        // odd len even len palindrome should be handle separate
-        // using wrap aroung center method
-        int startIdx = 0, len = 1;
-        int n = s.length();
-        for (int i = 0; n > i; i++) {
+        int n = s.size();
+
+        int start = 0;
+        int len = 1;
+
+        for (int i = 0; i < n; i++) {
             int l = i, r = i;
-            while (l >= 0 and n > r and s[l] == s[r]) {
-                if ((r + 1 - l) > len) {
-                    startIdx = l;
-                    len = r + 1 - l;
+
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > len) {
+                    len = r - l + 1;
+                    start = l;
                 }
                 l--;
                 r++;
             }
-            l = i, r = i + 1;
-            while (l >= 0 and n > r and s[l] == s[r]) {
-                if ((r + 1 - l) > len) {
-                    startIdx = l;
-                    len = r + 1 - l;
-                }
 
-                r++;
+            l = i;
+            r = i + 1;
+
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > len) {
+                    len = r - l + 1;
+                    start = l;
+                }
                 l--;
+                r++;
             }
         }
-        return s.substr(startIdx, len);
+
+        return s.substr(start, len);
     }
 };
+
