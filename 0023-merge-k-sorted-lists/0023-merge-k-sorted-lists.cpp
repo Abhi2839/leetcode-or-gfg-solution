21219362cf3eccb8770fb91bcae5bdc6f4d2a8bd
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-08-01 18:44:53 +0530

    Time: 0 ms (100%), Space: 19.2 MB (14.8%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,30 +10,40 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int>ans;
-             for (ListNode* list : lists) {  // to get each element of each list
-            while (list) {
-               ans.push_back(list->val);
-                list = list->next;
-            }
-        }
-        // ListNode temp=lists;
-        // while(temp){
-        //     ans.push_back(temp->val);
-        //     temp=temp->next;
-        // }
-        sort(ans.begin(),ans.end());
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* ans = &dummy;
 
-        if (ans.empty())
-        return nullptr;
-        ListNode*new_node =new ListNode(ans[0]);
-        ListNode*curr=new_node;
-  
-        for (int i=1;ans.size()>i;i++){
-           curr->next=new ListNode(ans[i]);
-           curr=curr->next;
+        while (l2 and l1) {
+            if (l1->val < l2->val) {
+                ans->next = l1;
+                l1 = l1->next;
+            } else {
+                ans->next = l2;
+                l2 = l2->next;
+            }
+            ans = ans->next;
         }
-        return new_node;
+        if (l1)
+            ans->next = l1;
+        if (l2)
+            ans->next = l2;
+        return dummy.next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty())
+            return nullptr;
+        if (lists.size() == 1)
+            return lists[0];
+        while (lists.size() > 1) {
+            vector<ListNode*> temp;
+            for (int i = 0; lists.size() > i; i += 2) {
+                ListNode* l1 = lists[i];
+                ListNode* l2 = lists.size() > i + 1 ? lists[i + 1] : nullptr;
+                temp.push_back(merge(l1, l2));
+            }
+            lists = move(temp);
+        }
+        return lists[0];
     }
 };
