46dcc08eabbdcca4666f5cb7585f5bc1261b0cf0
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-08-08 14:45:06 +0530

    Time: 8 ms (21.58%), Space: 14.3 MB (40.1%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> curr;
-    void solve(vector<int>& arr, int tgt, int idx) {
-        if (tgt == 0) {
-            ans.push_back(curr);
+    set<vector<int>> ans;
+    void solve(int i, vector<int>& arr, int target, vector<int>& s) {
+        if (target == 0) {
+            // sort(s.begin(), s.end());
+            ans.insert(s);
             return;
         }
-        if (0 > tgt)
+        if (target < 0)
             return;
-        for (int i = idx; arr.size() > i; i++) {
-            curr.push_back(arr[i]);
-            tgt -= arr[i];
-            solve(arr, tgt, i);
-            curr.pop_back();
-            tgt += arr[i];
+        for (int j = i; arr.size() > j; j++) {
+            s.push_back(arr[j]);
+            solve(j, arr, target - arr[j], s);
+            s.pop_back();
         }
     }
-    vector<vector<int>> combinationSum(vector<int>& arr, int tgt) {
-        solve(arr, tgt, 0);
-        return ans;
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        vector<int> ar;
+        sort(arr.begin(), arr.end());
+        solve(0, arr, target, ar);
+
+        return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
