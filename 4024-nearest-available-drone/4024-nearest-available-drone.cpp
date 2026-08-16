c352afa99b950960e0cd4f9ea3328cb8bb32e4d4
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-08-16 13:56:15 +0530

    Time: 4 ms (5.85%), Space: 55.4 MB (19.13%) - LeetHub

--- 4024-nearest-available-drone/4024-nearest-available-drone.cpp
+++ 4024-nearest-available-drone/4024-nearest-available-drone.cpp
@@ -5,7 +5,8 @@ public:
         int idx = -1;
 
         for (int i = 0; i < drones.size(); i++) {
-            int dist = abs(t[0] - drones[i][0]) + abs(t[1] - drones[i][1]);
+            int dist = abs(t[0] - drones[i][0])
+                     + abs(t[1] - drones[i][1]);
 
             if (dist <= drones[i][2] && minDist > dist) {
                 minDist = dist;
