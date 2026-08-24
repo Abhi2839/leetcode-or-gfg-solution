18c36163dac87a8336caa704d13012f771cc22bd
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-08-24 09:51:23 +0530

    Time: 3 ms (55.56%), Space: 9.4 MB (44.44%) - LeetHub

--- 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isPalindromic(string s) {
-        string str="";
-        for (auto x:s){
-          str+=bitset<8>(x).to_string();
+        string str = "";
+        for (auto x : s) {
+            str += bitset<8>(x).to_string();
         }
-        int i=0,j=str.size()-1;
-        while(j>i){
-            if (str[j]!=str[i]) return 0;
+        int i = 0, j = str.size() - 1;
+        while (j > i) {
+            if (str[j] != str[i])
+                return 0;
             j--;
             i++;
         }
