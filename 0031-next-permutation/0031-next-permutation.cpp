2fb148c0b9d76b7f7c7f0f6c5ee18116667f1ed8
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-09-01 22:13:24 +0530

    Time: 0 ms (100%), Space: 15.8 MB (15.06%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) {
+        int i = arr.size() - 2;
+        
+        while (i >= 0 && arr[i] >= arr[i + 1])
+            i--;
+
+        if (i >= 0) {
+            int j = arr.size() - 1;
+            while (arr[i] >= arr[j]) {
+                j--;
+            }
+            swap(arr[i], arr[j]);
+        }
+
+        reverse(arr.begin() + i + 1, arr.end());
+    }
+};
