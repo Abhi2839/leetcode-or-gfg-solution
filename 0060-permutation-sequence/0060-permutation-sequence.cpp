6549088c2d204db3724877ed7f2cb50a31455a0f
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-09-18 19:15:43 +0530

    Time: 1598 ms (5%), Space: 9 MB (11.13%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
+    string ans = "";
+
+    void rec(int n, int &k, string s) {
+        if (s.length() == n) {
+            k--;
+
+            if (k == 0) {
+                ans = s;
+            }
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            char ch = char(i + '0');
+            // to prevent duplicacy
+            if (s.find(ch) != string::npos)
+                continue;
+
+            s.push_back(ch);
+
+            rec(n, k, s);
+
+            if (!ans.empty())
+                return;
+
+            s.pop_back();
+        }
+    }
+
     string getPermutation(int n, int k) {
-
-        string s;
-
-        for (int i = 1; i <= n; i++)
-            s += to_string(i);
-
-        while (--k)
-            next_permutation(s.begin(), s.end());
-
-        return s;
+        rec(n, k, "");
+        return ans;
     }
 };
